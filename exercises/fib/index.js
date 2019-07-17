cb5bcc67ff6186df34bbf1538e66644ba4aeb28a
github username <omidvatan94@gmail.com>
2019-07-17 11:23:57 -0400

    fib recursive solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -10,17 +10,25 @@
 
 //iterative solution
 
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (i=2; i<=n; i++){
+//
+//     const a = result[i-1];
+//     const b = result[i-2];
+//
+//     result.push(a + b)
+//   }
+//   return result[n]
+// }
+
 function fib(n) {
-  const result = [0, 1];
-
-  for (i=2; i<=n; i++){
-
-    const a = result[i-1];
-    const b = result[i-2];
-
-    result.push(a + b)
+  if (n < 2){       
+    return n;
   }
-  return result[n]
+
+  return fib(n-1) + fib(n-2)
 }
 
 module.exports = fib;
