b04b4a0686367b7d246ff1e491bb8c01f8500744
github username <omidvatan94@gmail.com>
2019-07-17 11:13:36 -0400

    iterative fib solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,8 +8,19 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+//iterative solution
 
+function fib(n) {
+  const result = [0, 1];
+
+  for (i=2; i<=n; i++){
+
+    const a = result[i-1];
+    const b = result[i-2];
+
+    result.push(a + b)
+  }
+  return result[n]
 }
 
 module.exports = fib;
